76693619fe398a292d033cb61447fd0d405e6da9
VasileiosAidonis <vaidonis@dundee.ac.uk>
2020-10-01 12:40:55 +0300

    commit #13 background_added

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 
 var progressMeter = document.querySelectorAll(".progressbar span");
 var progressNumber = document.querySelectorAll('.progressbar span.bar_html .progressbar span.bar_css .progressbar span.bar_javascript .progressbar span.bar_php');
-var progressNumberWidths = [100,100,100,100,100,100,100,100,100,100];
+var progressNumberWidths = [101,101,101,100,100,100,100,101,100,101];
 var moveTimers = [];
 var width = 0;
 
@@ -44,31 +44,31 @@ function numberAddCheker(i) {
            document.getElementById('html-value-python').innerHTML = width + "%";
         }
         if (width <= progressNumberWidths[1]) {
-           document.getElementById('html-value-html').innerHTML = width + "%";
+           document.getElementById('html-value-python2').innerHTM = width + "%";
         }
         if (width <= progressNumberWidths[2]) {
-           document.getElementById('html-value-css').innerHTML = width + "%";
-        }
-        if (width <= progressNumberWidths[3]) {
-           document.getElementById('html-value-javascript').innerHTML = width + "%";
-        }
-        if (width <= progressNumberWidths[4]) {
            document.getElementById('html-value-php').innerHTML = width + "%";
         }
+        if (width <= progressNumberWidths[3]) {
+           document.getElementById('html-value-php2').innerHTML = width + "%";
+        }
+        if (width <= progressNumberWidths[4]) {
+           document.getElementById('html-value-html').innerHTML = width + "%";
+        }
         if (width <= progressNumberWidths[5]) {
-           document.getElementById('html-value-python2').innerHTML = width + "%";
+           document.getElementById('html-value-html2').innerHTML = width + "%";
         }
         if (width <= progressNumberWidths[6]) {
-           document.getElementById('html-value-html2').innerHTML = width + "%";
+           document.getElementById('html-value-css').innerHTML = width + "%";
         }
         if (width <= progressNumberWidths[7]) {
            document.getElementById('html-value-css2').innerHTML = width + "%";
         }
         if (width <= progressNumberWidths[8]) {
-           document.getElementById('html-value-javascript2').innerHTML = width + "%";
+           document.getElementById('html-value-javascript').innerHTML = width + "%";
         }
         if (width <= progressNumberWidths[9]) {
-           document.getElementById('html-value-php2').innerHTML = width + "%";
+           document.getElementById('html-value-javascript2').innerHTML = width + "%";
         }
     }
 }
